71c9b302a9ea1267b7af7d4e94cd37d0a9b2aac6
lcdavison <luke.davison@talktalk.net>
2019-03-13 13:55:34 +0000

    Load CSV file

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
-def main ():
+import csv
 
+#TODO: Scrub the data, there's a lot of redundant values
+#TODO: Split the dataset into Training and Test sets
+
+def main ( ):
+    with open ( "..\dataset.csv", 'r' ) as f:
+        reader = csv.reader ( f )
+        for row in reader:
+            print ( "# ROW %d %s\n" % ( reader.line_num, row ) )
+
+main ( )
