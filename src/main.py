cdca14b1dcc57a21c3f25e554fceeea4c24208f1
lcdavison <luke.davison@talktalk.net>
2019-03-13 18:13:55 +0000

    Added training and test sets

--- src/main.py
+++ src/main.py
@@ -3,10 +3,22 @@ import csv
 #TODO: Scrub the data, there's a lot of redundant values
 #TODO: Split the dataset into Training and Test sets
 
-def main ( ):
-    with open ( "..\dataset.csv", 'r' ) as f:
+#   Reads the contents of a dataset into the data array
+def read_dataset ( filename, training_set = [ ], test_set = [ ] ):
+    with open ( filename, 'r' ) as f:
         reader = csv.reader ( f )
-        for row in reader:
-            print ( "# ROW %d %s\n" % ( reader.line_num, row ) )
+        data = list ( reader )
+        training_set.append ( data [ 0:3680 ] )
+        test_set.append ( data [ 3681:4600 ] )
+        # print ( data )
+
+def main ( ):
+    training = [ ]  #   Contains Training Set
+    test = [ ]      #   Contains Test Set
+
+    read_dataset ( "..\dataset.csv", training, test )
+
+    for row in training:
+        print ( row )
 
 main ( )
